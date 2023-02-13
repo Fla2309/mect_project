87f117bacd42de2ab53971d1c120b0e7c32ec22c
Flavio Almanza <flavio.almanza01@gmail.com>
2023-02-13 15:57:09 -0600

    Updated login layout

--- view/login.php
+++ view/login.php
@@ -6,7 +6,7 @@
     <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
     <link rel="icon" href="img/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="../bootstrap-4.5.2-dist/css/bootstrap.css">
-	<link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/main.css">
 </head>
 
 <body id="background">
@@ -18,28 +18,52 @@
                 </div>
                 <?php if (isset($errorLogin))
                     echo "<div class=\"alert alert-danger\" style=\"font-size: .8rem;\" role=\"alert\">" . $errorLogin . "</div>" ?>
-                <label class="login_text">
-                    Usuario
-                </label>
-                <div>
-                    <img src="../img/user-icon.png" alt="" class="icon">
-                    <input name="username" class="login_input" type="text" placeholder="ejemplo@correo.com">
-                </div>
+                    <!--<label class="login_text">
+                                    Usuario
+                                </label>
+                                <div>
+                                    <img src="../img/user-icon.png" alt="" class="icon">
+                                    <input name="username" class="login_input" type="text" placeholder="ejemplo@correo.com">
+                                </div>
 
-                <label class="login_text">
-                    Contraseña
-                </label>
-                <div>
-                    <img src="../img/lock-icon.png" alt="" class="icon">
-                    <input name="password" class="login_input" type="password" placeholder="Contraseña123">
+                                <label class="login_text">
+                                    Contraseña
+                                </label>
+                                <div>
+                                    <img src="../img/lock-icon.png" alt="" class="icon">
+                                    <input name="password" class="login_input" type="password" placeholder="Contraseña123">
+                                </div>
+                                <div>
+                                    <input class="bg-primary" type="submit" value="INGRESAR" id="submit">
+                                </div>-->
+
+                    <div class="align-items-center mb-4 d-flex">
+                        <div class="login_icon">
+                            <img src="../img/user-icon.png" alt="" class="icon">
+                        </div>
+                        <div class="form-floating flex-fill ml-1">
+                            <input name="username" class="form-control login_input" id="userInput"
+                                placeholder="name@example.com">
+                            <label for="userInput">Usuario</label>
+                        </div>
+                    </div>
+                    <div class="align-items-center d-flex">
+                        <div class="login_icon">
+                            <img src="../img/lock-icon.png" alt="" class="icon">
+                        </div>
+                        <div class="form-floating flex-fill ml-1">
+                            <input name="password" type="password" class="form-control login_input" id="passInput"
+                                placeholder="Password">
+                            <label for="passInput">Contraseña</label>
+                        </div>
+                    </div>
+                    <div>
+                        <input class="bg-primary" type="submit" value="INGRESAR" id="submit">
+                    </div>
+                    <a href="#">Olvidé mi contraseña</a>
                 </div>
-                <div>
-                    <input class="bg-primary" type="submit" value="INGRESAR" id="submit">
-                </div>
-                <a href="#" style="font-family: ibm plex sans;">Olvidé mi contraseña</a>
             </div>
-        </div>
-    </form>
-</body>
+        </form>
+    </body>
 
-</html>
+    </html>
