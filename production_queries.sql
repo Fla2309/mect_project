062b94ff7c08ec941a8d3ed156605d2e49d9dcdf
Flavio Almanza <flavio.almanza@autozone.com>
2024-12-21 16:01:31 -0600

    added push notifications on presentations

--- production_queries.sql
+++ production_queries.sql
@@ -24,3 +24,7 @@ INSERT INTO trabajos_usuarios (id_trabajo, id_usuario)
 SELECT tm.id_trabajo, u.id
 FROM trabajos_modulos tm, usuarios u
 WHERE u.id_grupo=10 AND tm.id_modulo > 2;
+
+
+select count(c.id), u.nombre, u.apellidos from coaching_usuarios c join usuarios u on c.id_usuario = u.id where u.id_grupo=8 and u.status=0 group by u.id;
+select count(c.id), u.nombre, u.apellidos from coaching_usuarios c join usuarios u on c.id_usuario = u.id where u.id=19;
