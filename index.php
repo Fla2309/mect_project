362efdb39c3af384c245090d7aa2e9f3b66211ed
Flavio Almanza <46758378+Fla2309@users.noreply.github.com>
2025-08-05 16:09:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,3 @@
 <?php
 
-include_once 'php/user.php';
-include_once 'php/session.php';
-
-
-$userSession = new Session();
-$user = new User();
-
-if(isset($_SESSION['session_id'])){
-    include_once 'php/home.php';
-}else if(isset($_POST['username']) && isset($_POST['password'])){
-    $userForm = $_POST['username'];
-    $passForm = $_POST['password'];
-
-    if($user->userExists($userForm, $passForm)){
-        $userSession->setCurrentUser($userForm);
-        $user->setUser($userForm);
-        $userSession->setCurrentUserId($user);
-        $userSession->setCurrentUserFullname($user);
-        $userSession->setCurrentUserPreferredName($user);
-        $userSession->setCurrentUserType($user);
-        $userSession->generateSessionId();
-
-        include_once 'php/home.php';
-    }else{
-        $errorLogin = "Nombre de usuario y/o contraseña es incorrecto";
-        include_once 'view/login.php';
-    }
-
-}else{
-    include_once 'view/login.php';
-}
-
-?>
+echo 'app corriendo';
